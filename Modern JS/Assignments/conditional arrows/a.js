4f369a56205f951a73ddaca3bc7709746d4ea611
Dali <medalisaidi009@gmail.com>
2025-02-08 22:28:57 +0100

    Conditional arrows

---
+++ Modern JS/Assignments/conditional arrows/a.js
@@ -0,0 +1,37 @@
+ //Problem 0:
+// const exampleFunction = (param) => {
+//     return param === "hello" ? "hello!!!" : "bye bye!"
+// }
+// console.log(exampleFunction("hello")) //Output : hello!!!
+// console.log(exampleFunction("not hello")) //Output : bye bye!
+    
+//Problem 1: 
+const agecheck = (age) => {
+    return age > 18 ? "You are good to go!" : "Sorry! You must be 18 or older!"
+}
+console.log(agecheck(14)) 
+
+
+//problem 2 : 
+const weather = (status) => {
+    return status === "raining" ? "Get your jacket!" : "No rain on today forecast"
+}
+console.log(weather("raining")) 
+
+//Problem 3:
+const evencheck = (num) => {
+    return num % 2 === 0 ? "That's an even number" : "That's an odd unmber"
+}
+console.log(evencheck(4)) 
+
+//Problem 4:
+const comparenums = (num1, num2) => {
+    return num1 > num2 ? `${num1} is more than ${num2}` : `${num1} is less than ${num2}`
+}
+console.log(comparenums(9, 5))
+
+
+    
+
+
+
